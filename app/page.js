d59b2f2dfb6cdc4ac901a1bf08f1b69fbce41330
Somi14 <somigour2002@gmail.com>
2024-07-11 18:34:07 +0530

    second commit

--- app/page.js
+++ app/page.js
@@ -1,11 +1,18 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h2>Heloo welcome back to my channel !</h2>
-      <Button>Subscribe</Button>
+    <div className="bg-black">
+      
+      <div className="flex items-center justify-center h-screen flex-col gap-4">
+        <h1 className="text-white font-bold text-4xl">"Ace Your Next Interview with AI-Powered Mock Sessions"</h1>
+      <img src="/iTECH-Dysart-1200.jpg" alt="Centered Image" className="max-w-full h-auto" />
+      <Link href={'/dashboard'}>
+      <Button className="bg-slate-50 text-black w-48 hover:bg-slate-300" >Start</Button>
+      </Link>
+    </div>
     </div>
   );
 }
